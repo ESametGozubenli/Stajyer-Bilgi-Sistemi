a3780edcc0fb0141aa671f747e0cfaab661a4dfd
Beyza Nur Kurt <beyzanurkurt2211@gmail.com>
2025-08-12 14:34:19 +0300

    Login demo bilgileri kaldırıldı

--- src/pages/LogIn/LoginPage.js
+++ src/pages/LogIn/LoginPage.js
@@ -24,7 +24,7 @@ function LoginPage() {
         }else{
           alert("Kullanıcı adı veya şifre yanlış")
         }
-        console.log("Giriş Bilgileri : " ,values);
+       
     
     }
     
